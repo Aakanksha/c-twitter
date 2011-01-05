5b364a3e9b7832af67947c62894b31144106c112
Buddy Williams <buddy@buddylee.org>
2011-01-04 22:52:42 -0500

    added comments containing the instructions to finish the app

--- src/twitter.c
+++ src/twitter.c
@@ -6,6 +6,24 @@
 
 #include <stdio.h>
 
+// ------------------------------------------
+// Process of making a Twitter post, finally!
+// ------------------------------------------
+// 1. Get request token
+// 		Url: http://api.twitter.com/oauth/request_token
+//		- Ensure to use Authentication header
+//		- Build required fields
+//		- Sign data with secret key: HMAC-SHA-1
+// 2. Get user authorization through browser
+// 		Url: http://api.twitter.com/oauth/authorize
+//		- more info at: http://oauth.net/core/1.0/#anchor9
+// 3. Prompt user for 7-digit pin
+// 4. Get access token
+//		Url: http://api.twitter.com/oauth/access_token
+//		- use pin in param oauth_verifier
+// 5. Make Twitter update
+// ------------------------------------------
+
 main ()
 {
 }
